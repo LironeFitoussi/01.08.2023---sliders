8bcee8f2735fdbf3f2ee4cefcecd096695523196
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2023-08-22 12:24:00 +0300

    Done

---
+++ 21.08.2023/sandbox.js
@@ -0,0 +1,11 @@
+// function chkIfLow(arr, num){
+        //     var isLower = false;
+        //     for (var i = 0; i < arr.length; i++){
+        //         if (arr[1] > num) {
+        //             isLower = false;
+        //         }
+        //         isLower = true;
+        //         break
+        //     }
+
+        // }
