7b019d5e64e501e7baca2a4280faa3b82970564f
LironeFitoussi <lironefit@icloud.com>
2023-09-01 12:15:15 +0300

    38 Done

--- August 2023/30.08.2023/Tasks/37/main.js
+++ August 2023/30.08.2023/Tasks/37/main.js
@@ -1,34 +1,32 @@
 var computersArr = [];
 
 function getComputerByUser() {
-    var count = +prompt("Pick a Number of Computers");
+  var count = +prompt("Pick a Number of Computers");
 
-    for (let i = 0; i < count; i++) {
-        computersArr.push (
-            {
-                company: prompt("Choose Company"),
-                model: prompt("Choose Model"),
-                weight: +prompt("Type Weight"),
-                inStock: confirm("in Stock? Click OK for 'yes' or Cancel for 'no'")
-            }
-        )        
-    }  
+  for (let i = 0; i < count; i++) {
+    computersArr.push({
+      company: prompt("Choose Company"),
+      model: prompt("Choose Model"),
+      weight: +prompt("Type Weight"),
+      inStock: confirm("in Stock? Click OK for 'yes' or Cancel for 'no'"),
+    });
+  }
 
-    printComputers()
-    
-    console.log(computersArr);
+  printComputers();
+
+  console.log(computersArr);
 }
 
 function printComputers() {
-    for (let i = 0; i < computersArr.length; i++) {
-        var computer = computersArr[i]
-        if (computer.inStock == true && computer.weight > 2 )
-        document.getElementById("mainDiv").innerHTML +=
-        `
-            <h1>Company: ${computer.company}</h1>
-            <h2>Model: ${computer.model}</h2>
-            <p>Weight: ${computer.weight} KG <p>
-            <span>is in Stock?: ${computer.inStock}</span>
-        `
+  for (let i = 0; i < computersArr.length; i++) {
+    var computer = computersArr[i];
+    if (computer.inStock == true && computer.weight > 2) {
+      mainDiv.innerHTML += `
+                        <h1>Company: ${computer.company}</h1>
+                        <h2>Model: ${computer.model}</h2>
+                        <p>Weight: ${computer.weight} KG <p>
+                        <span>is in Stock?: ${computer.inStock}</span>
+                    `;
     }
+  }
 }
---
+++ August 2023/30.08.2023/Tasks/38/main.js
@@ -0,0 +1,33 @@
+var teachersArr = [];
+
+function getTecherByUser() {
+  var count = +prompt("Pick a Number of Teachers");
+
+  for (let i = 0; i < count; i++) {
+    teachersArr.push({
+      name: prompt("Choose Teacher's Name"),
+      hourSallary: +prompt("Set Teacher's Sallary Hourly"),
+      mail: prompt("Add Teacher's E-Mail:"),
+      birthYear: +prompt("What is the Teacher's Year of Birth?"),
+    });
+  }
+
+  printTeachers();
+}
+
+function printTeachers() {
+  for (let i = 0; i < teachersArr.length; i++) {
+    var teacher = teachersArr[i];
+    if (teacher.birthYear <= 1990 || teacher.hourSallary >= 100) {
+      teacherDiv.innerHTML += `
+            <span>
+                Name: ${teacher.name}
+                Sallry: ${teacher.hourSallary} Per Hour
+                E-Mail: ${teacher.mail}
+                Birth Year: ${teacher.birthYear}
+            </span>
+            <br>
+            `;
+    }
+  }
+}
