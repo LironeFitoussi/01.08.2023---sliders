805dcc9b1ecb906f760fd6335733c1777347d82d
LironeFitoussi <lironefit@icloud.com>
2023-09-02 23:13:06 +0300

    11.8 done

--- August 2023/31.08.2023/Tasks/11.2-4/main.js
+++ August 2023/31.08.2023/Tasks/11.2-4/main.js
@@ -1,9 +1,9 @@
 function openSearch() {
-    document.body.innerHTML += 
-    `<input type="text" id="searchInput" placeholder="Enter a name to search..." oninput="showNames()" ">
+  document.body.innerHTML += `
+    <input type="text" id="searchInput" placeholder="Enter a name to search..." oninput="showNames()" style="margin: 10px;">
     <div id="results"></div>
-    `
-    document.getElementsByTagName("button")[0].setAttribute("onclick", "")
+  `
+  document.getElementsByTagName("button")[0].setAttribute("onclick", "")
 }
 
 function logMsg() {
@@ -14,37 +14,44 @@ function displayByLength() {
   names.sort((a, b) => a.length - b.length)
   for (let i = 0; i < names.length; i++) {
     document.body.innerHTML += `
-      <p>${names[i]} ${names[i].length} </p>
+      <p style="font-size: 16px;">${names[i]} ${names[i].length} </p>
     `
   }
 }
 
+function displayByABC() {
+  names.sort()
+  for (let i = 0; i < names.length; i++) {
+    document.body.innerHTML += `
+      <p style="font-weight: bold;">${names[i]}</p>
+    `
+  }
+}
 
 var names = [
-    "Sophia Johnson",
-    "Liam Martinez",
-    "Olivia Smith",
-    "Noah Williams",
-    "Ava Anderson",
-    "Ethan Davis",
-    "Isabella Taylor",
-    "Aiden Jackson",
-    "Mia Brown",
-    "Elijah Miller"
-  ];
- 
-  function showNames() {
-    var findInput = searchInput.value;
-    for (let i = 0; i < names.length; i++) {
-      if (currentValue != findInput) {
-        results.innerHTML = ``;
-        currentValue = findInput;
-      } else if (names[i].indexOf(findInput) > -1 && findInput.length > 0) {
-        results.innerHTML += `
-        <p>${names[i]}</p>
-        `
-      } 
+  "Sophia Johnson",
+  "Liam Martinez",
+  "Olivia Smith",
+  "Noah Williams",
+  "Ava Anderson",
+  "Ethan Davis",
+  "Isabella Taylor",
+  "Aiden Jackson",
+  "Mia Brown",
+  "Elijah Miller"
+];
+
+function showNames() {
+  var findInput = searchInput.value;
+  for (let i = 0; i < names.length; i++) {
+    if (currentValue != findInput) {
+      results.innerHTML = ``;
+      currentValue = findInput;
+    } else if (names[i].indexOf(findInput) > -1 && findInput.length > 0) {
+      results.innerHTML += `
+        <p style="color: blue;">${names[i]}</p>
+      `
     }
-    var currentValue = findInput;
   }
-  
+  var currentValue = findInput;
+}
