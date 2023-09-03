b4eedaeade7819ef53e5c2f88a5aff3c2b0cfd5f
LironeFitoussi <lironefit@icloud.com>
2023-09-03 12:33:27 +0300

    2 Done

---
+++ September 2023/03.09.2023/2/main.js
@@ -0,0 +1,14 @@
+console.log("Test");
+
+document.body.innerHTML += `
+        <p></p>
+    `
+
+function printLastName() {
+    var lastName = document.getElementsByTagName("input")[0];
+    if (lastName.value != "") {
+        document.getElementsByTagName("p")[0].innerText = `${lastName.value}`
+        lastName.value = null
+    }
+    
+}
