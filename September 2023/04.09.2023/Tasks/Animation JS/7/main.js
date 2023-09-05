92e993e4ed610e0cd052d8de442bccd359917401
LironeFitoussi <lironefit@icloud.com>
2023-09-05 22:59:51 +0300

    Set Random Color Change

--- September 2023/04.09.2023/Tasks/Animation JS/7/main.js
+++ September 2023/04.09.2023/Tasks/Animation JS/7/main.js
@@ -1,11 +1,43 @@
 mainDiv.innerHTML = `
     <h1>Enter Colors & Text</h1>
-    <input type="text" id="colorInput1" placeholder="Color 1">
-    <input type="text" id="colorInput2" placeholder="Color 2">
-    <input type="text" id="colorInput3" placeholder="Color 3">
-    <input type="text" id="colorInput4" placeholder="Color 4">
-    <input type="text" id="colorInput5" placeholder="Color 5">
+    <input type="color" id="colorInput1" placeholder="Color 1">
+    <input type="color" id="colorInput2" placeholder="Color 2">
+    <input type="color" id="colorInput3" placeholder="Color 3">
+    <input type="color" id="colorInput4" placeholder="Color 4">
+    <input type="color" id="colorInput5" placeholder="Color 5">
     <input type="text" id="textInput" placeholder="טקסט">
-    <button>Submit</button>
+    <button id="printBtn">Submit</button>
+    <button id="stopBtn">Stop</button>
+
     <div id="output"></div>
 `
+printBtn.addEventListener("click", startPrint)
+// stopBtn.addEventListener("click", stopPrint)
+
+
+var colorArray = [] ;
+var radnomColorPrint;
+
+function startPrint() {
+    radnomColorPrint = setInterval(printText, 1000)
+    output.innerHTML = `
+        <p>${document.getElementsByTagName("input")[5].value}</p>
+    `
+}
+
+function printText() {
+    var randomColorIndex = Math.floor(Math.random()*4)
+    setColorArray();
+    document.getElementsByTagName("p")[0].style = `color: ${colorArray[randomColorIndex]}`
+}
+
+function setColorArray() {
+    for (let i = 0; i < 5; i++) {
+        colorArray.push(document.getElementsByTagName("input")[i].value)
+    }
+}
+
+
+
+
+
