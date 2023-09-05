b79736ea6675b4dc14d27f5ebe8eeb6c7b1b83ec
LironeFitoussi <lironefit@icloud.com>
2023-09-05 15:22:42 +0300

    2 Done

--- September 2023/04.09.2023/Tasks/Animation JS/1/main.js
+++ September 2023/04.09.2023/Tasks/Animation JS/1/main.js
@@ -3,18 +3,24 @@ console.log("Test");
 var heightCount = 0
 
 mainDiv.innerHTML = `
+    <button id="startBtn">Start Animation</button>
     <div class="colorDiv""></div>
 `
-
+startBtn.addEventListener("click", startAnimation)
 document.head.innerHTML += `
     <style> 
         .colorDiv {
             background-color: blue;
             width: 30vw;
+            height: 5vh
         }
     </style>  
 `
-setInterval(heightGrow, 1)
+
+function startAnimation() {
+    setInterval(heightGrow, 1)
+}
+
 
 function heightGrow() {
     if (heightCount <= 300) {
