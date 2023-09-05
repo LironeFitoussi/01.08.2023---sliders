d5f26543ec97fdd3e2f9e0d2ad932721e2a948ab
LironeFitoussi <lironefit@icloud.com>
2023-09-05 14:53:29 +0300

    UPDT

--- September 2023/04.09.2023/Class Tasks/1/main.js
+++
@@ -1,9 +0,0 @@
-myDiv.innerHTML += `
-    <h1>Click Me</h1>
-`
-document.body.getElementsByTagName("h1")[0].addEventListener("mouseover", sendAlert)
-
-function sendAlert() {
-    alert("h1 Clicked!")
-}
-
--- September 2023/04.09.2023/Tasks/setTimeout/1/main.js
+++ September 2023/04.09.2023/Tasks/setTimeout/1/main.js
@@ -1,3 +1,9 @@
-mainPage.innerHTML = `
-    <div width></div>
+myDiv.innerHTML += `
+    <h1>Click Me</h1>
 `
+document.body.getElementsByTagName("h1")[0].addEventListener("mouseover", sendAlert)
+
+function sendAlert() {
+    alert("h1 Clicked!")
+}
+
