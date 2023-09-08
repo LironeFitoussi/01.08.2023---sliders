433817049c61a56d0ce1891f0937f8e990f227c9
LironeFitoussi <lironefit@icloud.com>
2023-09-08 11:13:03 +0300

    process

--- September 2023/07.09.2023/Task 1/main.js
+++ September 2023/07.09.2023/Task 1/main.js
@@ -1,5 +1,36 @@
 console.log("test");
 
+function loadSite() {
+    var userName = logName.value
+    var userAge = new Date(userLogAge)
+    mainContainer.innerHTML = `
+    <h1 class="site-header display-1">Welcome back ${userName}</h1>
+    <div id="userInput">
+        <div class="input-group input-group-sm mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Please insert a Task:</span>
+            <input id="userTaskInput" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="input-group input-group-sm mb-3">
+            <label class="input-group-text" id="inputGroup-sizing-sm">Insert DeadLine:</label>
+            <input id="userDateInput" type="date" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupSelect01">Importance:</label>
+            <select class="form-select" id="inputGroupSelect01">
+              <option selected>Choose...</option>
+              <option value="Hight">Hight</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
+            </select>
+        </div>
+
+        <div class="buttons">
+            <button class="button-53" onclick="addTask()">Save this Task</button>
+            <button class="button-35" onclick="printUserTasks()" >Print Tasks</button>
+        </div>
+    </div>
+    `
+}
 var userTasks = [];
 
 function addTask() {
