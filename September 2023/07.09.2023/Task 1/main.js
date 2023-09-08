bbb4d55e2ccb62c855ac2658e47e2fbddc50e130
LironeFitoussi <lironefit@icloud.com>
2023-09-08 12:25:20 +0300

    Validation empty Proccess

--- September 2023/07.09.2023/Task 1/main.js
+++ September 2023/07.09.2023/Task 1/main.js
@@ -1,14 +1,26 @@
-console.log("test");
 
 function loadSite() {
-    var userName = logName.value
-    var userAge = new Date(userLogAge)
+    var season;
+    var userName = document.getElementById('logName').value;
+    var userBirthday = new Date(document.getElementById('userLogAge').value);
+    var currentTime = new Date();
+
+    // Age calculator
+    var userAge = currentTime.getFullYear() - userBirthday.getFullYear();
+    console.log(userAge);
+
+    var mainContainer = document.getElementById('mainContainer');
+
     mainContainer.innerHTML = `
-    <h1 class="site-header display-1">Welcome back ${userName}</h1>
+    <h1 class="site-header display-1">Welcome back ${userName} <span><img src"${season}"></span></h1>
     <div id="userInput">
         <div class="input-group input-group-sm mb-3">
-            <span class="input-group-text" id="inputGroup-sizing-sm">Please insert a Task:</span>
+            <span class="input-group-text" id="inputGroup-sizing-sm">Please insert the Task Name:</span>
             <input id="userTaskInput" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="input-group input-group-sm mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Task Description</span>
+            <input id="userTaskDescInput" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
         </div>
         <div class="input-group input-group-sm mb-3">
             <label class="input-group-text" id="inputGroup-sizing-sm">Insert DeadLine:</label>
@@ -24,6 +36,16 @@ function loadSite() {
             </select>
         </div>
 
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupSelect01">Status:</label>
+            <select class="form-select" id="inputGroupSelect02">
+              <option selected>Choose...</option>
+              <option value="Pending">Pending</option>
+              <option value="On Progress">On Progress</option>
+              <option value="Completed">Completed</option>
+            </select>
+        </div>
+
         <div class="buttons">
             <button class="button-53" onclick="addTask()">Save this Task</button>
             <button class="button-35" onclick="printUserTasks()" >Print Tasks</button>
@@ -31,29 +53,36 @@ function loadSite() {
     </div>
     `
 }
+
 var userTasks = [];
 
 function addTask() {
-  var validInput = true;
-  if (inputGroupSelect01.value == "") {
-    validInput = false;
-  }
-  if (validInput == true) {
-    console.log("Test");
-    var userTask = userTaskInput.value;
-    var userDate = userDateInput.value;
-    var userImportance = inputGroupSelect01.value;
+    var invalidTaskName = userTaskInput.value == ``
+    var invalidTaskDesc = userTaskDescInput.value == ``
+    var invalidTaskName = userTaskInput.value == ``
+    var invalidTaskName = userTaskInput.value == ``
+    var invalidTaskName = userTaskInput.value == ``
+
+    if (invalidTaskName || invalidTaskDesc) {
+        console.log("Test");
+        return
+    }
+    var userDate = document.getElementById('userDateInput').value;
+    var userImportance = document.getElementById('inputGroupSelect01').value;
 
     userTasks.push({
-      task: userTask,
+      task: userTaskInput.value,
+      taskDesk: document.getElementById('userTaskDescInput').value,
       date: userDate,
       importance: userImportance,
+      status: document.getElementById('inputGroupSelect02').value
     });
 
-    userTaskInput.value = ``;
-    userDateInput.value = ``;
-    inputGroupSelect01.value = ``;
-  }
+    userTaskInput.value = '';
+    userTaskDescInput.value = '';
+    userDateInput.value = '';
+    inputGroupSelect01.value = '';
+    inputGroupSelect02.value = '';
 }
 
 var tableExist = false;
@@ -64,8 +93,10 @@ function printUserTasks() {
             <table class="table">
                 <thead class="thead-dark">
                     <th scope="col">Task</th>
+                    <th scope="col">Description</th>
                     <th scope="col">Day</th>
                     <th scope="col">Importance</th>
+                    <th scope="col" >Status</th>
                 </thead>
                 <tbody id="userTasksOutput">
                 </tbody>
@@ -74,17 +105,19 @@ function printUserTasks() {
 
     tableExist = true;
   } else {
-    userTasksOutput.innerHTML = ``;
+    userTasksOutput.innerHTML = '';
   }
 
   for (let i = 0; i < userTasks.length; i++) {
-    console.log("Test");
     userTasksOutput.innerHTML += `
-            <tr>
+            <tr id="row${i}">
                 <td scope="row">${userTasks[i].task}</td>
+                <td scope="row">${userTasks[i].taskDesk}</td>
                 <td scope="row">${userTasks[i].date}</td>
-                <td scope="row">${userTasks[i].importance}</td>
+                <td scope="row"><span class="${userTasks[i].importance}">${userTasks[i].importance}</span></td>
+                <td scope="row">${userTasks[i].status}</td>
             </tr>
         `;
   }
 }
+
