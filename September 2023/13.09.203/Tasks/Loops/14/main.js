18efc1ea4c4f8a387e642cb324992eab4751abc2
LironeFitoussi <lironefit@icloud.com>
2023-09-13 22:11:36 +0300

    14 Done

---
+++ September 2023/13.09.203/Tasks/Loops/14/main.js
@@ -0,0 +1,56 @@
+console.log("Test");
+const people = [
+    {
+      firstName: "Adam",
+      lastName: "Cohen",
+      dateOfBirth: "01/01/1990",
+      email: "adam@example.com"
+    },
+    {
+      firstName: "Sarah",
+      lastName: "Levy",
+      dateOfBirth: "15/03/1985",
+      email: "sarah@example.com"
+    },
+    {
+      firstName: "Joseph",
+      lastName: "Cohen",
+      dateOfBirth: "30/07/1995",
+      email: "yosef@example.com"
+    },
+    {
+      firstName: "Rachel",
+      lastName: "Abramovich",
+      dateOfBirth: "10/11/1980",
+      email: "rachel@example.com"
+    }
+  ];
+
+  function printFullTable() {
+    placeContainer.innerHTML = `
+        <table>
+            <thead> 
+                <tr id="tableHeader"><tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    `
+
+    // set th's
+    for (const key in people[0]) {
+        document.querySelector("#tableHeader").innerHTML += `
+            <th>${key}</th>
+        `
+    }
+    for (const person of people) {
+        document.querySelector("tbody").innerHTML += `
+            <tr id="human_row_${people.indexOf(person)}"></tr>
+        `
+        for (const key in person) {
+            document.querySelector(`#human_row_${people.indexOf(person)}`).innerHTML += `
+                <td>${person[key]}</td>
+            `
+        }
+    }
+  }
