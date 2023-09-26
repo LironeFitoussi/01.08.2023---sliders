9a6f2cc4a8d44a2d5004e4ac7f6c447c84448f23
LironeFitoussi <lironefit@icloud.com>
2023-09-26 09:57:45 +0300

    1.b Done

--- September 2023/26.09.2023/Homwwork/main.js
+++ September 2023/26.09.2023/Homwwork/main.js
@@ -1,1 +1,34 @@
 console.log("test");
+// Task 1
+
+function Person(firstName, lastName, birthdate, profileImage) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = new Date(birthdate);
+    this.profileImage = profileImage;
+    this.getFullName = () => this.firstName 
+    this.getAge = () => {
+        const today = new Date();
+        const yearsDiff = today.getFullYear() - this.birthDate.getFullYear();
+        let hasBirthdayOccurred = new Boolean
+        if (today.getMonth() > this.birthDate.getMonth()) {
+            hasBirthdayOccurred = true
+        } else if (today.getMonth() == this.birthDate.getMonth()) {
+            console.log(today.getMonth() == this.birthDate.getMonth());
+            if (today.getDate() >= this.birthDate.getDate()){
+                hasBirthdayOccurred = true
+            } else {
+                hasBirthdayOccurred = false
+            }
+        } else {
+            hasBirthdayOccurred = false
+        }
+        const age = hasBirthdayOccurred ? yearsDiff : yearsDiff - 1;
+        return age;
+    };
+};
+
+const person1 = new Person("John", "Miller", "sep 26 1985", "AnuUrls")
+console.log(person1);
+console.log(person1.getFullName());
+console.log(person1.getAge());
