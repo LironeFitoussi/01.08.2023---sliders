38dc9d820efef90e5f72bda9cbc908d3113d0446
LironeFitoussi <lironefit@icloud.com>
2023-09-29 10:29:02 +0300

    progress

--- September 2023/moviesProject/JavaScript/main.js
+++ September 2023/moviesProject/JavaScript/main.js
@@ -1,22 +1,38 @@
-console.log("Test");
-function fetchMovies(categ ="popular") {
-    fetch(`https://api.themoviedb.org/3/movie/${categ}?language=en-US&page=1&api_key=df5fe72df0888382e4148e21f58c70b8`)
+function fetchMovies() {
+    fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=df5fe72df0888382e4148e21f58c70b8`)
     .then(response => response.json())
     .then(data => {
-        const moviesArr = data.results
-        console.log(moviesArr);
-        const backdropPath = moviesArr[0].backdrop_path
-        const posterSource = moviesArr[0].poster_path
-        const testImg = document.createElement("img")
-        testImg.src = `http://image.tmdb.org/t/p/w500${posterSource}`
-        document.body.innerHTML += testImg.outerHTML
-        testImg.src = `http://image.tmdb.org/t/p/w500${backdropPath}`
-        document.body.innerHTML += testImg.outerHTML
+        const moviesArr = data.results;
+        const tenTopMovies = new Array;
+        for (let i = 0; i < 10; i++) {
+            tenTopMovies.push(moviesArr[i]);
+        } 
+        tenTopMovies.forEach(movie => {
+            console.log(movie);
+            const newDiv = $("<div>");
+            newDiv.addClass("topMovies");
+            newDiv.css("background-image", `url(http://image.tmdb.org/t/p/w500${movie.backdrop_path})`);
+            newDiv.append(`
+                <div class="movieContent">
+                    <h1>${movie.original_title}<h1>
+                </div>
+            `)
+            $("header").append(newDiv);
+        });
+          
+        // console.log(tenTopMovies);
+        // const posterPath = moviesArr[0].poster_path;
+        // const posterImg = document.createElement("img");
+        // posterImg.src = `http://image.tmdb.org/t/p/w500${posterPath}`;
+        // $("main").append(posterImg);
+
+        // const backdropPath = moviesArr[0].backdrop_path;
+        // const backdropImg = document.createElement("img");
+        // backdropImg.src = `http://image.tmdb.org/t/p/w500${backdropPath}`;
+        // $("main").append(backdropImg);
     })
     .catch(err => console.error(err));
-
-    console.log();
 }
 
-fetchMovies("top_rated")
+fetchMovies();
   
