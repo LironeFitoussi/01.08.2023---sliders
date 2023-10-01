1cfdad348174431ebe32e6b7045daca31b25fcd7
LironeFitoussi <lironefit@icloud.com>
2023-10-01 20:25:24 +0300

    Week$Day Updated lik

--- September 2023/moviesProject/JavaScript/main.js
+++ September 2023/moviesProject/JavaScript/main.js
@@ -1,219 +1,246 @@
-let chosenFilter = "3/movie/popular"
-let favArr = new Array
+let chosenFilter = "day";
+let favArr = new Array();
 
 if (localStorage.userFavMovies != []) {
-    favArr = JSON.parse(localStorage.userFavMovies)
+  favArr = JSON.parse(localStorage.userFavMovies);
 }
-function fetchMovies(page =1, time ="3/movie/popular") {
-    fetch(`https://api.themoviedb.org/${time}?language=en-US&page=${page}&api_key=df5fe72df0888382e4148e21f58c70b8`)
-    .then(response => response.json())
-    .then(data => {
-        //! Header
-        const moviesArr = data.results;
-        const tenTopMovies = [];
+function fetchMovies(time = "day") {
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: `https://api.themoviedb.org/3/trending/movie/${time}?language=en-US&page=1`,
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjVmZTcyZGYwODg4MzgyZTQxNDhlMjFmNThjNzBiOCIsInN1YiI6IjY1MTVjNWQ0ZDQ2NTM3MDBjNjdiMmMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2huECgDm-vl-quAM-E50mrdEymLQO4yHgmgrdfRa3vI",
+    },
+  };
 
-        for (let i = 0; i < 10; i++) {
-            tenTopMovies.push(moviesArr[i]);
+  $.ajax(settings).done(function (data) {
+    console.log(data);
+    //! Header
+    const moviesArr = data.results;
+    const tenTopMovies = [];
+
+    for (let i = 0; i < 10; i++) {
+      tenTopMovies.push(moviesArr[i]);
+    }
+
+    const genrePromise = fetchMovieGenres();
+
+    let slideCount = 1;
+    $("#rightSlide").click(function autoSlideRight() {
+      console.log(slideCount);
+      if (slideCount == 10) {
+        slideCount = 1;
+      }
+      $(`#topMovies_${++slideCount}`)[0].scrollIntoView({ behavior: "smooth" });
+    });
+
+    $("#leftSlide").click(() => {
+      if (slideCount == 1) {
+        slideCount = 11;
+      }
+      --slideCount;
+      $(`#topMovies_${slideCount}`)[0].scrollIntoView({ behavior: "smooth" });
+    });
+
+    tenTopMovies.forEach((movie, index) => {
+      const movieDate = new Date(movie.release_date);
+      const newDiv = $("<div>");
+      const movieContent = $("<div>");
+      const movieTitle = movie.title;
+      newDiv.addClass("topMovies");
+      newDiv.attr("id", `topMovies_${index + 1}`);
+      movieContent.addClass("movieContent");
+
+      // Genre Promise
+      genrePromise.then((genres) => {
+        const genre = genres.find((genre) => genre.id === movie.genre_ids[0]);
+        if (genre) {
+          movieContent.find(".yearAndGenre span:last-child").text(genre.name);
         }
 
-        const genrePromise = fetchMovieGenres();
-
-        let slideCount = 1;
-        $("#rightSlide").click(function autoSlideRight() {
-            console.log(slideCount);
-            if (slideCount == 10) {
-                slideCount = 1
-            }
-            $(`#topMovies_${++slideCount}`)[0].scrollIntoView({behavior: 'smooth'});
-            
-        })
-        
-        $("#leftSlide").click(() => {
-            if (slideCount == 1) {
-                slideCount = 11
-            }
-            --slideCount
-            $(`#topMovies_${slideCount}`)[0].scrollIntoView({behavior: 'smooth'});
-        })
-        
-        tenTopMovies.forEach((movie, index) => {
-            const movieDate = new Date(movie.release_date);
-            const newDiv = $("<div>");
-            const movieContent = $("<div>");
-            const movieTitle = movie.title;
-            newDiv.addClass("topMovies");
-            newDiv.attr("id", `topMovies_${index+1}`)
-            movieContent.addClass("movieContent");
-
-            // Genre Promise
-            genrePromise.then(genres => {
-                const genre = genres.find(genre => genre.id === movie.genre_ids[0]);
-                if (genre) {
-                    movieContent.find(".yearAndGenre span:last-child").text(genre.name);
-                }
-
-                if (movieTitle.length > 18) {
-                    movieContent.find("h1").addClass("longTitle");
-                }
-            });
-
-            movieContent.append(`
-                <h1>${movieTitle}</h1>
-                <div class="yearAndGenre">
-                    <span>${movieDate.getFullYear()}</span>
-                    <i class="fa-solid fa-circle" style="color: #ffffff;"></i>
-                    <span></span>
-                </div>
-                <p>${movie.overview}</p>
-            `);
-
-            newDiv.css("background-image", `url(http://image.tmdb.org/t/p/original${movie.backdrop_path})`);
-            newDiv.append(movieContent);
-            $(".moviesSlider").append(newDiv);
-
-            
-
-            //? setInterval(() => {
-            //?    if (slideCount == 10) {
-            //?         slideCount = 1
-            //?     }
-            //?     $(`#topMovies_${++slideCount}`)[0].scrollIntoView({behavior: 'smooth'});
-            //? }, 5000);
-        });
-
-        //! Main
-        nextPage(1, chosenFilter)
-
-        //! Pagination
-        console.log(data);
-        const paginationElem = $("<div>");
-        paginationElem.addClass("pagination")
-        for (let i = 1; i <= 5; i++) {
-            const newPageBtn = $("<a>")
-            newPageBtn.addClass("pageBtn")
-            newPageBtn.text(i)   
-            newPageBtn.attr("value", i)
-            paginationElem.append(newPageBtn)
+        if (movieTitle.length > 18) {
+          movieContent.find("h1").addClass("longTitle");
         }
-        const lastPage = $("<a>")
-        lastPage.text(250)
-        lastPage.attr("value", 6)
-        lastPage.addClass("pageBtn")
-        paginationElem.append(lastPage)
-        
-        $("main").append(paginationElem)
-        $(paginationElem).insertAfter("main");
-        
-        const pageBtnArr = document.querySelectorAll(".pageBtn")
-        pageBtnArr[0].classList.add("active")
+      });
 
-        //! Pagination Listener
-        pageBtnArr.forEach((page) => {
-            page.addEventListener("click", () => {
-                pageBtnArr[clickedBtn-1].classList.remove("active")
-                clickedBtn = page.getAttribute("value")
-                page.classList.add("active")
-                nextPage(page.innerText, chosenFilter)
-            })
-        })
+      movieContent.append(`
+            <h1>${movieTitle}</h1>
+            <div class="yearAndGenre">
+                <span>${movieDate.getFullYear()}</span>
+                <i class="fa-solid fa-circle" style="color: #ffffff;"></i>
+                <span></span>
+            </div>
+            <p>${movie.overview}</p>
+        `);
 
-        $("#chooseFilter").on("submit", (e) => {
-            e.preventDefault();
-            chosenFilter = $("#timeFilter").val();
-            pageBtnArr[clickedBtn-1].classList.remove("active")
-            clickedBtn = 1
-            pageBtnArr[0].classList.add("active")
-            nextPage(1, chosenFilter);
-        })
-    })
-    .catch(err => console.error(err));
+      newDiv.css(
+        "background-image",
+        `url(http://image.tmdb.org/t/p/original${movie.backdrop_path})`
+      );
+      newDiv.append(movieContent);
+      $(".moviesSlider").append(newDiv);
+
+      //? setInterval(() => {
+      //?    if (slideCount == 10) {
+      //?         slideCount = 1
+      //?     }
+      //?     $(`#topMovies_${++slideCount}`)[0].scrollIntoView({behavior: 'smooth'});
+      //? }, 5000);
+    });
+
+    //! Main
+    nextPage(1, chosenFilter);
+
+    //! Pagination
+    console.log(data);
+    const paginationElem = $("<div>");
+    paginationElem.addClass("pagination");
+    for (let i = 1; i <= 5; i++) {
+      const newPageBtn = $("<a>");
+      newPageBtn.addClass("pageBtn");
+      newPageBtn.text(i);
+      newPageBtn.attr("value", i);
+      paginationElem.append(newPageBtn);
+    }
+    const lastPage = $("<a>");
+    lastPage.text(250);
+    lastPage.attr("value", 6);
+    lastPage.addClass("pageBtn");
+    paginationElem.append(lastPage);
+
+    $("main").append(paginationElem);
+    $(paginationElem).insertAfter("main");
+
+    const pageBtnArr = document.querySelectorAll(".pageBtn");
+    pageBtnArr[0].classList.add("active");
+
+    //! Pagination Listener
+    pageBtnArr.forEach((page) => {
+      page.addEventListener("click", () => {
+        pageBtnArr[clickedBtn - 1].classList.remove("active");
+        clickedBtn = page.getAttribute("value");
+        page.classList.add("active");
+        nextPage(page.innerText, chosenFilter);
+      });
+    });
+
+    $("#chooseFilter").on("submit", (e) => {
+      e.preventDefault();
+      chosenFilter = $("#timeFilter").val();
+      pageBtnArr[clickedBtn - 1].classList.remove("active");
+      clickedBtn = 1;
+      pageBtnArr[0].classList.add("active");
+      nextPage(1, chosenFilter);
+    });
+  });
 }
 
 function fetchMovieGenres() {
-    return fetch('https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=df5fe72df0888382e4148e21f58c70b8')
-        .then(response => response.json())
-        .then(data => data.genres)
-        .catch(err => {
-            console.error(err);
-            return []; 
-        });
+  return fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=df5fe72df0888382e4148e21f58c70b8"
+  )
+    .then((response) => response.json())
+    .then((data) => data.genres)
+    .catch((err) => {
+      console.error(err);
+      return [];
+    });
 }
 
-let clickedBtn = 1
-function nextPage(page =1, time) {
-    window.scrollTo({
-        top: 800,
-        behavior: 'smooth'
-    });
-    fetch(`https://api.themoviedb.org/${time}?language=en-US&page=${page}&api_key=df5fe72df0888382e4148e21f58c70b8`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        $("main").html("")
-        const thisPageMovies = data.results;
-        thisPageMovies.map((selectedMovie, movieIndex) => {
-            const movieCard = $("<div>")
-            movieCard.addClass("movieCard")
-            const newMovie = $("<img>");
-            newMovie.addClass("movieImg")
-            const movieDate = new Date(selectedMovie.release_date);
+let clickedBtn = 1;
 
-            // hiddenSlide
-            const slideInfo = $("<div>")
-            const movieTitle = selectedMovie.title
-            
-            slideInfo.html(`
+function nextPage(page = 1, time) {
+  window.scrollTo({
+    top: 800,
+    behavior: "smooth",
+  });
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: `https://api.themoviedb.org/3/trending/movie/${time}?language=en-US&page=${page}`,
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjVmZTcyZGYwODg4MzgyZTQxNDhlMjFmNThjNzBiOCIsInN1YiI6IjY1MTVjNWQ0ZDQ2NTM3MDBjNjdiMmMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2huECgDm-vl-quAM-E50mrdEymLQO4yHgmgrdfRa3vI",
+    },
+  };
+
+  $.ajax(settings).done(function (data) {
+      console.log(data);
+      $("main").html("");
+      const thisPageMovies = data.results;
+      thisPageMovies.map((selectedMovie, movieIndex) => {
+        const movieCard = $("<div>");
+        movieCard.addClass("movieCard");
+        const newMovie = $("<img>");
+        newMovie.addClass("movieImg");
+        const movieDate = new Date(selectedMovie.release_date);
+
+        // hiddenSlide
+        const slideInfo = $("<div>");
+        const movieTitle = selectedMovie.title;
+
+        slideInfo.html(`
                 <h1>${movieTitle}</h1>
                 <span>${movieDate.getFullYear()}</span>
                 <p>${selectedMovie.overview}</p>
-            `)
+            `);
 
-            // Favorite
-            const mainStar = $("<div>")
-            mainStar.addClass(`fa-star fa-regular`)
-            mainStar.css("color", "#fbd723")
-            slideInfo.append(mainStar)
+        // Favorite
+        const mainStar = $("<div>");
+        mainStar.addClass(`fa-star fa-regular`);
+        mainStar.css("color", "#fbd723");
+        slideInfo.append(mainStar);
 
-            // check if already Fav
-            favArr.map(movie => {
-                if (movie.id == selectedMovie.id) {
-                    mainStar.addClass(`fa-solid`)
-                }
-            })
-
-            if (movieTitle.length > 15) {
-                slideInfo.find("h1").css("font-size", "2vw");
-            }
-
-            slideInfo.addClass("slideInfo")
-
-            movieCard.attr("id", `movie_${movieIndex+1}`)
-            newMovie.attr("src", `http://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`)
-            $(movieCard).append( slideInfo, newMovie)
-
-            $("main").append(movieCard)
-        })
-
-        let starsArr = document.querySelectorAll(".fa-star");
-        starsArr.forEach((star, index) => {
-            star.addEventListener("click", () => {
-                let favorite = star.classList.contains("fa-solid");
-                if (favorite) {
-                    let keyToRemove = "id";
-                    let valueToRemove = data.results[index].id;      
-                    star.classList.remove("fa-solid");
-                    favArr = favArr.filter(item => item[keyToRemove] !== valueToRemove)
-                } else {
-                    favArr.push(data.results[index]);
-                    star.classList.add("fa-solid");
-                }
-                let localFavorites = JSON.stringify(favArr)
-                localStorage.setItem("userFavMovies", localFavorites)
-            });
+        // check if already Fav
+        favArr.map((movie) => {
+          if (movie.id == selectedMovie.id) {
+            mainStar.addClass(`fa-solid`);
+          }
         });
+
+        if (movieTitle.length > 15) {
+          slideInfo.find("h1").css("font-size", "2vw");
+        }
+
+        slideInfo.addClass("slideInfo");
+
+        movieCard.attr("id", `movie_${movieIndex + 1}`);
+        newMovie.attr(
+          "src",
+          `http://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`
+        );
+        $(movieCard).append(slideInfo, newMovie);
+
+        $("main").append(movieCard);
+      });
+
+      let starsArr = document.querySelectorAll(".fa-star");
+      starsArr.forEach((star, index) => {
+        star.addEventListener("click", () => {
+          let favorite = star.classList.contains("fa-solid");
+          if (favorite) {
+            let keyToRemove = "id";
+            let valueToRemove = data.results[index].id;
+            star.classList.remove("fa-solid");
+            favArr = favArr.filter(
+              (item) => item[keyToRemove] !== valueToRemove
+            );
+          } else {
+            favArr.push(data.results[index]);
+            star.classList.add("fa-solid");
+          }
+          let localFavorites = JSON.stringify(favArr);
+          localStorage.setItem("userFavMovies", localFavorites);
+        });
+      });
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 }
 
-
-fetchMovies(1, chosenFilter);
+fetchMovies(chosenFilter);
