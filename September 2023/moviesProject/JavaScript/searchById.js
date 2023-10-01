bd444ac90dbbf9c1a37dbce03a55670abafadc99
LironeFitoussi <lironefit@icloud.com>
2023-10-01 10:22:22 +0300

    progress movie trailer

--- September 2023/moviesProject/JavaScript/searchById.js
+++ September 2023/moviesProject/JavaScript/searchById.js
@@ -22,24 +22,35 @@ const settings = {
     // main Movie Details
     const mainMovieInfo = $("<article>");
     const releaseDate = new Date(response.release_date);
+    const runTime = new Date(response.runtime*60*60)
+    const trailerIdPromise = fetchVideoTrailer(movieId)
+    trailerIdPromise.then(data => {
+        console.log(data);
+    })
+
     console.log(releaseDate.getFullYear());
     console.log(mainMovieInfo);
     mainMovieInfo.addClass("mainMovie")
     mainMovieInfo.append(
         `
-            <div>
+            <div >
                 <h1>${response.title}</h1>
-                <span>${(releaseDate.getFullYear())}</span>
+                <span>${(releaseDate.getFullYear())} | Duration: ${parseInt(runTime.getTime()/60/60/60) + "h" + runTime.getTime()/60/60%60} </span> <hr>
+                <span id="movieDirector"></span>
                 <p>${response.overview}</p>
+                <a href="https://www.imdb.com/title/${response.imdb_id}">
+                    <img id="imdbLogo" src="../Assets/IMDB_Logo_2016.svg.png">
+                </a>
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/35?si=4ZJBUdvHa240g8LV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <img id="moviePoster" src=http://image.tmdb.org/t/p/original${response.poster_path}></img>
         `
     )
     $("main").append(mainMovieInfo)
 
-    //? add IMDB link
-    //? Serch Rotten Tomatoes link
-    //? add directors
+    //* add IMDB link
+    //!! Serch Rotten Tomatoes link
+    //* add directors
     //? Serach Youtube Trailers
     //? Add Similar 
     //? find Movie Duration
@@ -51,7 +62,13 @@ const settings = {
     actors.addClass("actorsCont");
     actorPromise.then(data => {
         console.log(data);
-        for (const int of data) {
+
+        // Director
+        const crewArr = data.crew;
+        const movieDirector = crewArr.find(crew => crew.job == "Director")
+        console.log(movieDirector.name);
+        $("#movieDirector").text("Directed By: " + movieDirector.name);
+        for (const int of data.cast) {
             const actorCard = $("<div>")
             actorCard.addClass("actorCard")
             const actorPic = $("<img>");
@@ -73,13 +90,12 @@ const settings = {
   });
 
   function fetchMovieCredits(movieId) {
-    const creditsUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`;
-
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`;
     return new Promise((resolve, reject) => {
         const settings = {
             async: true,
             crossDomain: true,
-            url: creditsUrl,
+            url: url,
             method: 'GET',
             headers: {
                 accept: 'application/json',
@@ -89,12 +105,37 @@ const settings = {
 
         $.ajax(settings)
             .done(response => {
-                resolve(response.cast);
+                resolve(response);
             })
             .fail(error => {
                 reject(error);
             });
     });
 }
+
+function fetchVideoTrailer(movieId) {
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`
+    return new Promise((resolve, reject) => {
+        const settings = {
+            async: true,
+            crossDomain: true,
+            url: url,
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjVmZTcyZGYwODg4MzgyZTQxNDhlMjFmNThjNzBiOCIsInN1YiI6IjY1MTVjNWQ0ZDQ2NTM3MDBjNjdiMmMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2huECgDm-vl-quAM-E50mrdEymLQO4yHgmgrdfRa3vI'
+            }
+        };
+          
+        $.ajax(settings).done(response => {
+            resolve(response);
+        })
+        .fail(error => {
+            reject(error);
+        });
+    })
+}
+
+fetchVideoTrailer(movieId)
 
 
