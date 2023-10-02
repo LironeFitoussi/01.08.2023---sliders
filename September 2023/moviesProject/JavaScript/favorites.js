dbb7f266ed5d8849347a785bb147b15f38b5d17f
LironeFitoussi <lironefit@icloud.com>
2023-10-02 09:08:56 +0300

    Favorites moduled

--- September 2023/moviesProject/JavaScript/favorites.js
+++ September 2023/moviesProject/JavaScript/favorites.js
@@ -1,76 +1,18 @@
 let favArr = new Array
-console.log(localStorage.userFavMovies);
-if (localStorage.userFavMovies != "[]") {
+
+// Navbar Loader Module
+import getNavBar from "./modules/navBar.js";
+getNavBar()
+
+// loadFav Loader Module
+import loadFav from "./modules/loadFav.js";
+
+if (localStorage.userFavMovies) {
     console.log(favArr);
     favArr = JSON.parse(localStorage.userFavMovies)
-    loadFav()
-}
-
-function loadFav() {
-    console.log("entered");
-        $("main").html("")
-        const thisPageMovies = favArr;
-        thisPageMovies.map((selectedMovie, movieIndex) => {
-            const movieCard = $("<div>")
-            movieCard.addClass("movieCard")
-            const newMovie = $("<img>");
-            newMovie.addClass("movieImg")
-            const movieDate = new Date(selectedMovie.release_date);
-
-            // hiddenSlide
-            const slideInfo = $("<div>")
-            const movieTitle = selectedMovie.title
-            
-            slideInfo.html(`
-                <h1>${movieTitle}</h1>
-                <span>${movieDate.getFullYear()}</span>
-                <p>${selectedMovie.overview}</p>
-            `)
-
-            // Favorite
-            const mainStar = $("<div>")
-            mainStar.addClass(`fa-star fa-regular`)
-            mainStar.css("color", "#fbd723")
-            slideInfo.append(mainStar)
-
-            // check if already Fav
-            favArr.map(movie => {
-                if (movie.id == selectedMovie.id) {
-                    mainStar.addClass(`fa-solid`)
-                }
-            })
-
-            if (movieTitle.length > 15) {
-                slideInfo.find("h1").css("font-size", "2vw");
-            }
-
-            slideInfo.addClass("slideInfo")
-
-            movieCard.attr("id", `movie_${movieIndex+1}`)
-            newMovie.attr("src", `http://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`)
-            $(movieCard).append( slideInfo, newMovie)
-
-            $("main").append(movieCard)
-        })
-
-        let starsArr = document.querySelectorAll(".fa-star");
-        starsArr.forEach((star, index) => {
-            star.addEventListener("click", () => {
-                let keyToRemove = "id";
-                let valueToRemove = favArr[index].id;      
-                star.classList.remove("fa-solid");
-                favArr = favArr.filter(item => item[keyToRemove] !== valueToRemove)
-                console.log(favArr);
-                // syncToLocalStorage
-                let localFavorites = JSON.stringify(favArr)
-                localStorage.setItem("userFavMovies", localFavorites)
-                location.reload();
-            });
-        });
+    loadFav(favArr)
 }
 
 
-$("#navbarDropdownMenuLink").click(function () {
-    let dropdown = $('#sbw');
-    dropdown.toggle()
-  });
+
+
---
+++ September 2023/moviesProject/JavaScript/modules/navBar.js
@@ -0,0 +1,55 @@
+export default function getNavBar() {
+    console.log("Nav Loaded");
+    const navContent = $("<nav>")
+    navContent.html(
+    `
+        <nav class="navbar navbar-expand-lg navbar-dark p-3 bg-danger" id="headerNav">
+            <div class="container-fluid">
+                <a class="navbar-brand d-block d-lg-none" href="#">
+                    <img src="../Assets/favicon.ico" height="80" />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 " aria-current="page" href="./index.html">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Search Movie
+                            </a>
+                            <ul id="sbw" class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="./singleById.html">By ID</a></li>
+                                <li><a class="dropdown-item" href="#">By Name</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item d-none d-lg-block">
+                            <a class="nav-link mx-2" href="#">
+                                <img src="../Assets/favicon.ico" height="80" />
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2" href="./about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 active" href="./favorites.html">Favorites</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `
+    );
+    $(navContent).insertBefore("header");
+    $("#navbarDropdownMenuLink").click(function () {
+    let dropdown = $('#sbw');
+    dropdown.toggle()
+    }) ;
+}
+
