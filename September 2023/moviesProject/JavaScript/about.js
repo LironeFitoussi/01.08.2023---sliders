f29f7cc375a9519a6d4a26d2124200b21e3d4808
LironeFitoussi <lironefit@icloud.com>
2023-10-03 18:18:38 +0300

    msg WA Fixed

--- September 2023/moviesProject/JavaScript/about.js
+++ September 2023/moviesProject/JavaScript/about.js
@@ -11,7 +11,7 @@ getFooter()
 $("#WAForm").submit(function (e) { 
     e.preventDefault();
     console.log("uloaded");
-    userMSG = $("#message").val()
+    let userMSG = $("#message").val()
     window.open(`https://api.whatsapp.com/send?phone=972585109829&text=${userMSG}`, '_blank');
 });
 
