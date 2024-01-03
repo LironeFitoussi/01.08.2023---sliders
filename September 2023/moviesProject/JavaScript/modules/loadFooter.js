80259efa587baf16983f3ecafa2223125f280d2a
LironeFitoussi <lironefit@icloud.com>
2024-01-03 09:26:49 +0200

    auto date update

--- September 2023/moviesProject/JavaScript/modules/loadFooter.js
+++ September 2023/moviesProject/JavaScript/modules/loadFooter.js
@@ -1,11 +1,11 @@
 export default function getFooter() {
-    console.log("footer Loaded");
-    const footerContent = $("<footer>");
-    footerContent.html(
+  console.log("footer Loaded");
+  const footerContent = $("<footer>");
+  const currentYear = new Date().getFullYear();
+  footerContent.html(
+    `
+        <p>&copy; ${currentYear} StreamSphere. All rights reserved. | Developed by Fistuk</p>
       `
-        <p>&copy; 2023 StreamSphere. All rights reserved. | Developed by Fistuk</p>
-      `
-    );
-    $(footerContent).insertAfter("main");
-  }
-  
+  );
+  $(footerContent).insertAfter("main");
+}
