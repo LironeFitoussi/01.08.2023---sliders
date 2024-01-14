200f28ad6c7b89f9f888f170935ce5a00244061b
LironeFitoussi <lironefit@icloud.com>
2024-01-14 17:33:26 +0200

    form validation added

--- January 2024/14.01.2024/otzaot-react/src/pages/Spends.jsx/Spends.jsx
+++ January 2024/14.01.2024/otzaot-react/src/pages/Spends.jsx/Spends.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import SpendItem from "../../components/SpendItem/SpendItem";
 import styles from './Spends.module.css'
 
@@ -11,7 +11,7 @@ export default function Spends() {
         title: '',
         category: '',
         amount: 0,
-        type: 'Income'
+        type: ''
     });
 
     const handleChange = (e) => {
@@ -24,19 +24,21 @@ export default function Spends() {
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault(); // Prevent default form submission
-
-        setSpends((prevSpends) => {
-            return [...prevSpends, newSpend]; // Directly push newSpend to the array
-        });
-
-        // Reset form after submission
-        setNewSpend({
-            title: '',
-            category: '',
-            amount: 0,
-            type: 'Income'
-        });
+        e.preventDefault();
+        const isValid = newSpend.title !== '' && newSpend.category !== '' && newSpend.amount !== 0 && newSpend.type !== ''
+        if (isValid) {
+            setSpends((prevSpends) => {
+                return [...prevSpends, newSpend];
+            });
+            setNewSpend({
+                title: '',
+                category: '',
+                amount: 0,
+                type: ''
+            });
+        } else {
+            return
+        }  
     };
 
     return (
@@ -58,18 +60,37 @@ export default function Spends() {
                 </tbody>
             </table>
             <form className={styles.spendsForm} onSubmit={handleSubmit}>
-                <h3>Add An action</h3>
+                <h3 style={{color: 'black'}}>Add An action</h3>
                 <label className={styles.inputLabel} htmlFor="title">Title:</label>
-                <input type="text" name='title' onChange={handleChange} value={newSpend.title} />
+                <input 
+                    type="text" 
+                    name='title' 
+                    onChange={handleChange} 
+                    value={newSpend.title} 
+                />
 
                 <label htmlFor="category">Category</label>
-                <input type="text" name='category' onChange={handleChange} value={newSpend.category} />
+                <input 
+                    type="text" 
+                    name='category' 
+                    onChange={handleChange}
+                    value={newSpend.category} 
+                />
 
                 <label htmlFor="amount">Amount</label>
-                <input type="number" name="amount" onChange={handleChange} value={newSpend.amount} />
+                <input 
+                    type="number" 
+                    name="amount" 
+                    onChange={handleChange} 
+                    value={newSpend.amount} 
+                />
 
                 <label htmlFor="type">Type</label>
-                <select name="type" onChange={handleChange} value={newSpend.type}>
+                <select 
+                name="type" 
+                onChange={handleChange} 
+                value={newSpend.type} 
+                required>
                     <option value="Income">Income</option>
                     <option value="Outcome">Outcome</option>
                 </select>
