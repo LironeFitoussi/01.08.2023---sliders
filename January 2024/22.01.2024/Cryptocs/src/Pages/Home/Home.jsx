e6ce9d968580d4e4edf5ef6df74ed8c2bd3092c9
LironeFitoussi <lironefit@icloud.com>
2024-01-22 14:03:01 +0200

    Home Page Basic Set

--- January 2024/22.01.2024/Cryptocs/src/Pages/Home/Home.jsx
+++ January 2024/22.01.2024/Cryptocs/src/Pages/Home/Home.jsx
@@ -1,7 +1,18 @@
 import styles from './Home.module.css'
-
+import { Link } from 'react-router-dom'
 export default function Home () {
     return (
-        <h1>This is Home Page</h1>
+        <section className={styles.container}>
+            <h1 className={styles.mainHeader}>Welcome to CRYPTOC, your next Crypto Manager.</h1>
+            <button className={styles.mainButton}>
+                <Link to="/auth" ><p style={{color: 'black'}}>Register NOW!</p></Link>
+            </button>
+            <img src="" alt="" />
+            <div>
+                <p><a href="">FACEBOOK</a></p>
+                <p><a href="">LINKEDIN</a></p>
+                <p><a href="">INSTAGRAM</a></p>
+            </div>
+        </section>
     )
 }
