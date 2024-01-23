1f74a02bb3828762cf5ccd77ed6f2ead238dc79a
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-23 16:46:30 +0200

    Delete functio nbutton done

--- January 2024/22.01.2024/Cryptocs/src/Components/Currency/Currency.jsx
+++ January 2024/22.01.2024/Cryptocs/src/Components/Currency/Currency.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import styels from './Currency.module.css';
+import styles from './Currency.module.css';
+import { Link } from 'react-router-dom';
+import { db } from '../../Config/firebase';
+import { getDocs, collection, query, where, deleteDoc } from 'firebase/firestore';
 
 export default function Currency({ id, rank, symbol, name, priceUsd, addToFavorite, fav }) {
     const [idToAdd, setIdToAdd] = useState('');
@@ -10,6 +13,22 @@ export default function Currency({ id, rank, symbol, name, priceUsd, addToFavori
         setButtonDisabled(true);
     }
 
+    const handleDelete = async () => {
+        try {
+            const q = query(collection(db, 'favorites'), where('currency', '==', id));
+            const querySnapshot = await getDocs(q);
+
+            querySnapshot.forEach(async (doc) => {
+                await deleteDoc(doc.ref);
+            });
+
+            console.log('Document deleted successfully!');
+        } catch (error) {
+            console.error('Error deleting document:', error);
+            // You can add additional error handling logic as needed
+        }
+    }
+
     useEffect(() => {
         if (idToAdd.length > 0) {
             addToFavorite(idToAdd);
@@ -17,7 +36,7 @@ export default function Currency({ id, rank, symbol, name, priceUsd, addToFavori
     }, [idToAdd]);
 
     return (
-        <div className={styels.cryptoContainer}>
+        <div className={styles.cryptoContainer}>
             <h1>{name}</h1>
             <span>{symbol}</span>
             <p>Rank: {rank}</p>
@@ -27,10 +46,14 @@ export default function Currency({ id, rank, symbol, name, priceUsd, addToFavori
                     Add to Favorites
                 </button>
                 :
-                <button>
+                <button onClick={handleDelete}>
                     Remove from Favorites
                 </button>
             }
+
+            <button>
+                <Link to={`/favorites/${id}`}>Expand</Link>
+            </button>
         </div>
     );
 }
--- January 2024/22.01.2024/Cryptocs/src/Pages/SingleCurrency/SingleCurrency.jsx
+++ January 2024/22.01.2024/Cryptocs/src/Pages/SingleCurrency/SingleCurrency.jsx
@@ -1,9 +1,109 @@
-import styles from './SingleCurrency.module.css'
+import styles from './SingleCurrency.module.css';
+import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 export default function SingleCurrency() {
+    const [cryptoData, setCryptoData] = useState();
+    const [coinHistory, setCoinHistory] = useState();
+    const [usdAmount, setUsdAmount] = useState(1); // Added state for usdAmount
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const apiKey = '93ceb527-91ae-44e4-961b-d826b53299b4';
+                const apiUrl = 'https://api.coincap.io/v2/assets/' + id;
+
+                const response = await fetch(apiUrl, {
+                    headers: {
+                        Authorization: `Bearer ${apiKey}`,
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+
+                const data = await response.json();
+                setCryptoData(data.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [id]);
+
+    useEffect(() => {
+        const fetchHistory = async () => {
+            try {
+                const apiKey = '93ceb527-91ae-44e4-961b-d826b53299b4';
+                const apiUrl = 'https://api.coincap.io/v2/assets/' + id + '/history?interval=' + 'd1';
+
+                const response = await fetch(apiUrl, {
+                    headers: {
+                        Authorization: `Bearer ${apiKey}`,
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+
+                const data = await response.json();
+                setCoinHistory(data.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchHistory();
+    }, [id]);
+
+    useEffect(() => {
+        cryptoData && console.log(cryptoData);
+        coinHistory && console.log(coinHistory);
+    }, [cryptoData, coinHistory]);
+
+    const handleUsdAmountChange = (e) => {
+        console.log(typeof e.target.value);
+        const amount = parseFloat(e.target.value);
+        if (amount < 0) {
+            setUsdAmount(0);
+        } else if (e.target.value.length === 0) {
+            setUsdAmount('empty')
+        } else {
+            setUsdAmount(amount);
+        }
+    };
+
     return (
-        <section>
-            <h1>Single Currency Page</h1>
-        </section>
-    )
+        cryptoData && (
+            <section className={styles.container}>
+                <div className={styles.currencyHeader}>
+                    <h1>{cryptoData.name}</h1>
+                    <span>{`(${cryptoData.symbol})`}</span>
+                    <p className={styles.change24}>{cryptoData.changePercent24Hr}</p>
+                </div>
+                <p className={styles.coinSupply}>
+                    Supply: {parseInt(cryptoData.supply).toFixed(2)} / {parseInt(cryptoData.maxSupply).toFixed(2)}
+                </p>
+                <h2>USD Value</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                <input
+                                    type="number"
+                                    value={usdAmount}
+                                    onChange={handleUsdAmountChange} // Added onChange handler
+                                />
+                            </th>
+                            <th>{typeof usdAmount === 'number' ? parseFloat(cryptoData.priceUsd * usdAmount).toFixed(2) : 'Please enter a valid Number'}</th>
+                        </tr>
+                    </thead>
+                </table>
+            </section>
+        )
+    );
 }
