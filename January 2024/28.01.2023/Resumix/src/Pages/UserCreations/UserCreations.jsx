0674dfad5f1065db30317e372aaf2bf1ff215d6b
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-01-29 15:51:52 +0200

    success fetching rsumes

--- January 2024/28.01.2023/Resumix/src/Pages/UserCreations/UserCreations.jsx
+++ January 2024/28.01.2023/Resumix/src/Pages/UserCreations/UserCreations.jsx
@@ -1,12 +1,31 @@
 import styles from './UserCreations.module.css'
-import { db } from '../../config/firebase'
 import userDataProvider from '../../context/UserData';
 import { useEffect, useState, useContext } from 'react'
-
+import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { db } from '../../config/firebase';
 
 export default function UserCreations() {
     const { currentUser } = useContext(userDataProvider);
-    console.log(currentUser);
+    const { user } = useContext(userDataProvider);
+    const [resumeData, setResumeData] = useState();
+
+    useEffect(() => {
+        console.log(user);
+        try {
+            const resumeCollectionRef = collection(db, 'resumes');
+            const resumeQuery = query(resumeCollectionRef, where('userId', '==', user.uid));
+
+            onSnapshot(resumeQuery, async (snapshot) => {
+                const resumeData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                console.log(resumeData);
+                setResumeData(resumeData);
+            });
+
+            console.log('Data fetched succefully!');
+        } catch (err) {
+            console.error('An error ocured: ' + err);
+        }
+    }, [user])
 
     return (
         <section>
