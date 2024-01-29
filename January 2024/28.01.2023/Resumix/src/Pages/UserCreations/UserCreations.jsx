063a26e2e9c66a2c25a9dc54c1ea059a7acb6836
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-01-29 15:23:57 +0200

    fix

--- January 2024/28.01.2023/Resumix/src/Pages/UserCreations/UserCreations.jsx
+++ January 2024/28.01.2023/Resumix/src/Pages/UserCreations/UserCreations.jsx
@@ -1,11 +1,17 @@
 import styles from './UserCreations.module.css'
 import { db } from '../../config/firebase'
 import userDataProvider from '../../context/UserData';
+import { useEffect, useState, useContext } from 'react'
+
 
 export default function UserCreations() {
+    const { currentUser } = useContext(userDataProvider);
+    console.log(currentUser);
+
     return (
         <section>
-            <h1>this is user creations page</h1>
+            <h1>This is user creations page</h1>
+            <p>User recent creations should be displayed here next:</p>
         </section>
     )
 }
