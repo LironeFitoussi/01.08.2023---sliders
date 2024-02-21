afbbcb74ed9df1458ee6dc5519e403b230a39908
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-21 11:01:48 +0200

    sever progression

--- Feburary 2024/E-Commerce API/Server/controllers/productsController.js
+++ Feburary 2024/E-Commerce API/Server/controllers/productsController.js
@@ -16,6 +16,19 @@ exports.getAllProducts = async (req, res) => {
   }
 };
 
+exports.createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 exports.getSingleProduct = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
