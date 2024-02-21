9c08eeac798fad7d43d9b3741929d09f6ff30bbb
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-21 13:15:56 +0200

    progression added fixed navbasr for admin

--- Feburary 2024/E-Commerce API/Client/src/pages/Admin/Admin.jsx
+++ Feburary 2024/E-Commerce API/Client/src/pages/Admin/Admin.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateProduct from './CreateProduct.jsx';
 import styles from './Admin.module.css';
-
+import { Link } from "react-router-dom";
+ 
 export default function Admin() {
     return (       
         <main className={styles.mainContainer}>              
-                <aside>
-                    <h1>Admin Panel</h1>
-                    <h3>👇Here is where all Admin Panel should be displayed👇</h3>
-                </aside>
+                
                 <Routes>
                     <Route path='/' element={
+                        <>
+                        <aside>
+                            <h1>Admin Panel</h1>
+                            <nav className={styles.nav}>
+                                <ul className={styles.ul}>
+                                    <li className={styles.li}>
+                                        <Link to="/" className={styles.link}>Home</Link>
+                                    </li>
+                                    <li className={styles.li}>
+                                        <Link to="/admin/create-product" className={styles.link}>Create Product</Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </aside>
                         <div className={styles.admin}>
                             <h1 className={styles.header}>Admin Page</h1>
-                        </div>
+                        </div></>
                     }/>
                     <Route path='/create-product' element={<CreateProduct />} />
                 </Routes>
