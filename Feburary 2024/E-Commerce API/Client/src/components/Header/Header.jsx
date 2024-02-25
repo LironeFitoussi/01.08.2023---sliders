5c29941cd2d4a23c830692c945420abf7a82ebe9
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-25 17:09:31 +0200

    Revert "progress adding to cart"
    This reverts commit d45c7c744426e97f9cf7a7e49c6ab86cde2937b0.

--- Feburary 2024/E-Commerce API/Client/src/components/Header/Header.jsx
+++ Feburary 2024/E-Commerce API/Client/src/components/Header/Header.jsx
@@ -1,67 +1,47 @@
 import { Link } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { UserContext } from "../../context/User";
 import SignOut from "../signout/signout";
 import styles from "./Header.module.css";
 
 export default function Header() {
-  const { userToken, user, fetchUser } = useContext(UserContext);
-  const [isLoading, setIsLoading] = useState(true);
+    const { userToken, user } = useContext(UserContext);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      await fetchUser();
-      setIsLoading(false);
-    };
-
-    getUserData();
-  }, [fetchUser]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <ul className={styles.ul}>
-          <li className={styles.li}>
-            <Link to="/" className={styles.link}>
-              Home
-            </Link>
-          </li>
-          <li className={styles.li}>
-            <Link to="/products" className={styles.link}>
-              Browse Store
-            </Link>
-          </li>
-          {!user ? ( // Check if user is not defined
-            <li className={styles.li}>
-              <Link to="/authentication" className={styles.link}>
-                Authentication
-              </Link>
-            </li>
-          ) : (
-            <>
-              <SignOut />
-              {user.role !== "admin" && ( // Display Cart only if user is not admin
-                <li className={styles.li}>
-                  <Link to={`/cart/${user._id}`} className={styles.link}>
-                    Cart
-                  </Link>
-                </li>
-              )}
-              {user.role === "admin" && ( // Check if user is admin
-                <li className={styles.li}>
-                  <Link to="/admin" className={styles.link}>
-                    Admin
-                  </Link>
-                </li>
-              )}
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <ul className={styles.ul}>
+                    <li className={styles.li}>
+                        <Link to="/" className={styles.link}>Home</Link>
+                    </li>
+                    <li className={styles.li}>
+                        <Link to="/products" className={styles.link}>Browse Store</Link>
+                    </li>
+                    {user && ( // Check if user exists
+                        <>
+                            {user._id !== undefined && !userToken ? ( // Check if user is authenticated
+                                <li className={styles.li}>
+                                    <Link to="/authentication" className={styles.link}>Authentication</Link>
+                                </li>
+                            ) : (
+                                <>
+                                    <SignOut />
+                                    {user.role !== "admin" && ( // Display Cart only if user is not admin
+                                        <li className={styles.li}>
+                                            <Link to={`/cart/${user._id}`} className={styles.link}>Cart</Link>
+                                        </li>
+                                    )}
+                                    {user.role === "admin" && ( // Check if user is admin
+                                        <li className={styles.li}>
+                                            <Link to="/admin" className={styles.link}>Admin</Link>
+                                        </li>
+                                    )}
+                                </>
+                            )}
+                        </>
+                    )}
+                </ul>
+            </nav>
+        </header>
+    );
 }
--- Feburary 2024/E-Commerce API/Server/routes/cartRoutes.js
+++ Feburary 2024/E-Commerce API/Server/routes/cartRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 
 const cartController = require("../controllers/cartController");
-const authController = require("../controllers/authController");
 
 const router = express.Router();
-router.use(authController.userValidator);
 
 router.route("/:userId").get(cartController.getCart);
+
 router.route("/:id/addToCart").patch(cartController.addToCart);
+
 router.route("/:id/removeFromCart").delete(cartController.removeFromCart);
+
 router.route("/:id/pay").post(cartController.pay);
 
 module.exports = router;
