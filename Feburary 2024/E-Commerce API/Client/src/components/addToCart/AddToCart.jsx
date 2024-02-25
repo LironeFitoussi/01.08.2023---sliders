ab2592fdf674b55d62e0f5210d10a1a6eb17c965
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-25 17:17:01 +0200

    adding to cart done without styling

---
+++ Feburary 2024/E-Commerce API/Client/src/components/addToCart/AddToCart.jsx
@@ -0,0 +1,33 @@
+import styles from "./AddToCart.module.css";
+import axios from "axios";
+// import { useParams } from "react-router-dom";
+import { UserContext } from "../../context/User";
+import { useContext } from "react";
+
+export default function AddToCart({ productId }) {
+  console.log(productId);
+  const { userToken } = useContext(UserContext);
+  //   const obj = useParams();
+  //   console.log(obj);
+  const handleAddToCart = () => {
+    let config = {
+      method: "patch",
+      maxBodyLength: Infinity,
+      url: `http://localhost:3000/api/v1/cart/${productId}/addToCart`,
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return <button onClick={handleAddToCart}>Add to Cart ((Cart icon))</button>;
+}
