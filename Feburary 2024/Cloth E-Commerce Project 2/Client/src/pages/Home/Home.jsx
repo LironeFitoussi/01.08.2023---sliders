f7f5076d5e96360313e89108f800d9e934b8be44
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-26 08:04:11 +0200

    Newsletter set

--- Feburary 2024/Cloth E-Commerce Project 2/Client/src/pages/Home/Home.jsx
+++ Feburary 2024/Cloth E-Commerce Project 2/Client/src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
 
 // import components
-import Navbar from "../../components/Navbar/Navbar"
-import Announcement from '../../components/Announcement/Announcement';
-import Slider from '../../components/Slider/Slider';
-import Categories from '../../components/Categories/Categories';
-import Products from '../../components/Products/Products';
-
-export default function Home () {
+import Navbar from "../../components/Navbar/Navbar";
+import Announcement from "../../components/Announcement/Announcement";
+import Slider from "../../components/Slider/Slider";
+import Categories from "../../components/Categories/Categories";
+import Products from "../../components/Products/Products";
+import Newsletter from "../../components/Newsletter/Newsletter";
+export default function Home() {
   return (
     <div>
-        <Announcement/>
-        <Navbar/>
-        <Slider/>
-        <Categories />
-        <Products/>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
     </div>
-  )
+  );
 }
