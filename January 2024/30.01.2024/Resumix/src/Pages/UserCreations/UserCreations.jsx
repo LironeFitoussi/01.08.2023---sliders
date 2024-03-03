e8ec7a6e2b2c4eed7d9b336fcc7b420ee22b5425
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-03-03 12:27:03 +0200

    front mobile done

--- January 2024/30.01.2024/Resumix/src/Pages/UserCreations/UserCreations.jsx
+++ January 2024/30.01.2024/Resumix/src/Pages/UserCreations/UserCreations.jsx
@@ -1,56 +1,71 @@
-import styles from './UserCreations.module.css'
-import userDataProvider from '../../context/UserData';
-import { useEffect, useState, useContext } from 'react'
-import { collection, query, where, onSnapshot, getDocs } from 'firebase/firestore';
-import { db } from '../../config/firebase';
-import UserResumes from '../../components/UserResumes/UserResumes';
+import styles from "./UserCreations.module.css";
+import userDataProvider from "../../context/UserData";
+import { useEffect, useState, useContext } from "react";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../../config/firebase";
+import UserResumes from "../../components/UserResumes/UserResumes";
 
 export default function UserCreations() {
-    const { currentUser } = useContext(userDataProvider);
-    const { user } = useContext(userDataProvider);
-    const [resumeData, setResumeData] = useState(null);
+  const { currentUser } = useContext(userDataProvider);
+  const { user } = useContext(userDataProvider);
+  const [resumeData, setResumeData] = useState(null);
 
-    console.log(currentUser);
-    useEffect(() => {
-        if (currentUser && user) {
-            if (currentUser.isAdmin) {
-                console.log("Admin user");
-                try {
-                    const resumeCollectionRef = collection(db, 'resumes')
-                    onSnapshot(resumeCollectionRef, async (snapshot) => {
-                        const resumeData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                        setResumeData(resumeData);
-                    });
-                } catch (error) {
-                    console.error(error);
-                }
-            } else {
-                console.log("Normal user");
-                try {
-                    const resumeCollectionRef = collection(db, 'resumes');
-                    const resumeQuery = query(resumeCollectionRef, where('userId', '==', user.uid));
-
-                    onSnapshot(resumeQuery, async (snapshot) => {
-                        const resumeData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                        setResumeData(resumeData);
-                    });
-
-                    console.log('Data fetched succefully!');
-                } catch (err) {
-                    console.error('An error ocured: ' + err);
-                }
-            }
+  console.log(currentUser);
+  useEffect(() => {
+    if (currentUser && user) {
+      if (currentUser.isAdmin) {
+        console.log("Admin user");
+        try {
+          const resumeCollectionRef = collection(db, "resumes");
+          onSnapshot(resumeCollectionRef, async (snapshot) => {
+            const resumeData = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            setResumeData(resumeData);
+          });
+        } catch (error) {
+          console.error(error);
         }
-    }, [currentUser, user])
+      } else {
+        console.log("Normal user");
+        try {
+          const resumeCollectionRef = collection(db, "resumes");
+          const resumeQuery = query(
+            resumeCollectionRef,
+            where("userId", "==", user.uid)
+          );
 
-    return (
-        <>
-            {currentUser && <section>
-                <h1>This is user creations page</h1>
-                <p>User recent creations should be displayed here next:</p>
-                {resumeData !== null && <UserResumes resumeData={resumeData} />}
-            </section>}
-        </>
+          onSnapshot(resumeQuery, async (snapshot) => {
+            const resumeData = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            setResumeData(resumeData);
+          });
 
-    )
+          console.log("Data fetched succefully!");
+        } catch (err) {
+          console.error("An error ocured: " + err);
+        }
+      }
+    }
+  }, [currentUser, user]);
+
+  return (
+    <>
+      {currentUser && (
+        <section>
+          <h1 className={styles.header}>This is user creations page</h1>
+          {resumeData !== null && <UserResumes resumeData={resumeData} />}
+        </section>
+      )}
+    </>
+  );
 }
--- January 2024/30.01.2024/Resumix/src/components/ResumeCard/ResumeCard.jsx
+++ January 2024/30.01.2024/Resumix/src/components/ResumeCard/ResumeCard.jsx
@@ -1,77 +1,80 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './ResumeCard.module.css';
-import dateFormat from 'dateformat'; // Make sure you have this library properly installed
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import styles from "./ResumeCard.module.css";
+import dateFormat from "dateformat"; // Make sure you have this library properly installed
 import { doc, deleteDoc } from "firebase/firestore";
-import { db } from '../../config/firebase.js'
-import userDataProvider from '../../context/UserData';
+import { db } from "../../config/firebase.js";
+import userDataProvider from "../../context/UserData";
 
 const ResumeCard = ({ resume }) => {
-    const [isHovered, setIsHovered] = useState(false);
-    const [dateFormatted, setDateFormatted] = useState('');
-    const { currentUser } = useContext(userDataProvider);
-    console.log(currentUser);
-    useEffect(() => {
-        // console.log(resume.creationDate);
-        const creationDate = new Date(resume.creationDate.seconds * 1000);
-        const formattedDate = isToday(creationDate)
-            ? dateFormat(creationDate, 'HH:MM')
-            : dateFormat(creationDate, 'longDate');
-        setDateFormatted(formattedDate);
-    }, [resume]);
+  const [isHovered, setIsHovered] = useState(false);
+  const [dateFormatted, setDateFormatted] = useState("");
+  const { currentUser } = useContext(userDataProvider);
+  console.log(currentUser);
+  useEffect(() => {
+    // console.log(resume.creationDate);
+    const creationDate = new Date(resume.creationDate.seconds * 1000);
+    const formattedDate = isToday(creationDate)
+      ? dateFormat(creationDate, "HH:MM")
+      : dateFormat(creationDate, "longDate");
+    setDateFormatted(formattedDate);
+  }, [resume]);
 
-    const isToday = (date) => {
-        const today = new Date();
-        return (
-            date.getDate() === today.getDate() &&
-            date.getMonth() === today.getMonth() &&
-            date.getFullYear() === today.getFullYear()
-        );
-    };
-
-    const deleteResume = async () => {
-        // todo: add delete logic
-        try {
-            await deleteDoc(doc(db, "resumes", resume.id));
-        } catch (error) {
-            console.error('An error ocured: ' + error);
-        }
-    }
-
-
+  const isToday = (date) => {
+    const today = new Date();
     return (
-        <div
-            className={styles.resumeCard}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            {resume && (
-                <div className={styles.editContainer} style={{ display: isHovered ? 'flex' : 'none' }}>
-                    <Link to={`/your-creations/edit/${resume.id}`} className={styles.editBtn}>
-                        <img src="./images/pen-solid.svg" alt="edit" />
-                    </Link>
-                    <Link
-                        className={styles.editBtn}
-                        to="/create/template"
-                        state={{ from: resume }}
-                    >
-                        <img src="./images/eye-solid.svg" alt="view" />
-                    </Link>
-                    <button onClick={deleteResume} className={styles.deleteBtn}>
-                        <img src="./images/trash-solid.svg" alt="view" />
-                    </button>
-                </div>
-            )}
-            <h3>{resume ? resume.fileName : 'Loading...'}</h3>
-
-            <p>{dateFormatted}</p>
-            {currentUser.isAdmin && <p
-                style={{
-                    fontSize: '.5vw'
-                }}
-            > User: {resume.userId}</p>}
-        </div>
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
     );
+  };
+
+  const deleteResume = async () => {
+    // todo: add delete logic
+    try {
+      await deleteDoc(doc(db, "resumes", resume.id));
+    } catch (error) {
+      console.error("An error ocured: " + error);
+    }
+  };
+
+  return (
+    <div
+      className={styles.resumeCard}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {resume && (
+        <div
+          className={styles.editContainer}
+          style={{ display: isHovered ? "flex" : "none" }}
+        >
+          <Link
+            to={`/your-creations/edit/${resume.id}`}
+            className={styles.editBtn}
+          >
+            <img src="./images/pen-solid.svg" alt="edit" />
+          </Link>
+          <Link
+            className={styles.editBtn}
+            to="/create/template"
+            state={{ from: resume }}
+          >
+            <img src="./images/eye-solid.svg" alt="view" />
+          </Link>
+          <button onClick={deleteResume} className={styles.deleteBtn}>
+            <img src="./images/trash-solid.svg" alt="view" />
+          </button>
+        </div>
+      )}
+      <h3>{resume ? resume.fileName : "Loading..."}</h3>
+
+      <p>{dateFormatted}</p>
+      {currentUser.isAdmin && (
+        <p className={styles.userInfo}> User: {resume.userId}</p>
+      )}
+    </div>
+  );
 };
 
 export default ResumeCard;
