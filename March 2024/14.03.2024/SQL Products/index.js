9b73a6e7c38052b636f8530af6ec95976a418a5c
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-03-26 23:07:30 +0200

    dsfgsdfg

--- March 2024/14.03.2024/SQL Products/index.js
+++ March 2024/14.03.2024/SQL Products/index.js
@@ -17,12 +17,14 @@ app.get("/", async (req, res) => {
   try {
     // Fetch items from the database
     const result = await db.query(
-      "SELECT * FROM products ORDER BY product_id ASC"
+      `
+            SELECT p.product_id, p.product_name, c.category_name, p.price, p.stock_quantity, p.description
+            FROM products p
+            JOIN categories c ON p.category_id = c.category_id
+            ORDER BY p.product_id ASC;
+        `
     );
-    const items = result.rows; // Assuming the column names match the properties of the item objects
-
-    // Log the retrieved items
-    // console.log(items);
+    const items = result.rows; // Assuming the column names match the properties of the item object
 
     // Render the index.ejs template with the retrieved items
     res.render("index", {
